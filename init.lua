e612aed59924c08704c30137c659750b965326fa
GJ Tiquia <gjtiquia@gmail.com>
2026-03-16 22:18:16 -0700

    wip alt+9 unity editor switch

--- init.lua
+++ init.lua
@@ -12,6 +12,12 @@ hs.hotkey.bind({ "alt", "shift" }, "b", selectAnyWindowOfCurrentApp)
 
 hs.hotkey.bind("alt", "2", openswitch("Zen"))
 
+-- TODO : dont launch if there is no existing Unity editor, should launch Unity Hub
+-- TODO : this works for one unity window, when two is open, it cant switch between
+-- TODO : also it cycles between windows in one unity editor, where i want to cycle between different unity editors
+-- TODO : however, alt+b is able to pickup the other Unity window, so likely we can hijack that and use that list
+hs.hotkey.bind("alt", "9", openswitch("Unity"))
+
 -- note:
 -- oddly, even for this "native" solution, only 1 zen window is available on load
 -- only when i hv focused on the other zen window at least once, only then will it appear on the list
